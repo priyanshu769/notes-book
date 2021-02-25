8aadad4d03f7194b0e8ed6d24dc207da94edc5b4
Priyanshu <74850965+priyanshu769@users.noreply.github.com>
2021-02-25 16:45:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [input, setInput] = useState({});
+  const [noteList, setNoteList] = useState([]);
+
+  const changeHandler = (x) => {
+    if (x.target.name === "heading") {
+      setInput({ ...input, haeding: x.target.value });
+    } else if (x.target.name === "note") {
+      setInput({ ...input, note: x.target.value });
+    } else if (x.target.name === "addNote") {
+      setNoteList([...noteList, input]);
+    }
+  };
+  console.log(input);
+  console.log(noteList);
+  return (
+    <div className="App">
+      <h1>NotesBook</h1>
+      <h2>Note down all stuffs at one place.</h2>
+      <div>
+        <input onChange={changeHandler} name="heading" />
+        <br />
+        <input onChange={changeHandler} name="note" />
+        <br />
+        <button onClick={changeHandler} name="addNote">
+          Add Note
+        </button>
+      </div>
+      {/* display notes */}
+      <div>
+        <ul></ul>
+      </div>
+    </div>
+  );
+}
