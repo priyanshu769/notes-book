b869b163de0b4ed181ab008bf45a9d8c3475b8ae
Priyanshu <74850965+priyanshu769@users.noreply.github.com>
2021-02-25 20:13:04 +0530

    Added delete feature

--- src/App.js
+++ src/App.js
@@ -1,29 +1,80 @@
 import { useState } from "react";
 import "./styles.css";
+import uuid from "react-uuid";
+
+// This was made to check the delete feature.
+// const notes = [
+//   {
+//     id: "1",
+//     heading: "Buy Vegies",
+//     note: "Tomato, Potato, Onion, Spinach"
+//   },
+//   {
+//     id: "2",
+//     heading: "Buy Protien",
+//     note: "Paneer, Meet, Eggs"
+//   },
+//   {
+//     id: "3",
+//     heading: "Buy Clothes",
+//     note: "Jacket, Jeans, Shirt, T-Shirt"
+//   }
+// ];
 
 export default function App() {
+  // const [empty, setEmpty] = useState("");
   const [input, setInput] = useState({});
   const [noteList, setNoteList] = useState([]);
 
   const changeHandler = (x) => {
     if (x.target.name === "heading") {
-      setInput({ ...input, heading: x.target.value });
+      setInput({ ...input, heading: x.target.value, id: "1" });
     } else if (x.target.name === "note") {
       setInput({ ...input, note: x.target.value });
     } else if (x.target.name === "addNote") {
       setNoteList([...noteList, input]);
+      setInput({});
     }
   };
-  console.log(input);
-  console.log(noteList);
+  // console.log(input);
+  // console.log(noteList);
+  const deleteNote = (x) => {
+    notes.map((delNote) => {
+      if (x.target.id == delNote.id) {
+        // console.log(delNote);
+        // console.log(noteList.indexOf(delNote));
+        // console.log(noteList.splice(noteList.indexOf(delNote), 1));
+        // notes.splice(notes.indexOf(delNote), 1);
+        setNoteList([
+          ...noteList,
+          noteList.splice(noteList.indexOf(delNote), 1)
+        ]);
+        // console.log(notes);
+      } else {
+        setNoteList([...noteList]);
+        // console.log(notes);
+      }
+    });
+  };
+
   return (
     <div className="App">
-      <h1>NotesBook</h1>
+      <h1>Notes' Book</h1>
       <h2>Note down all stuffs at one place.</h2>
       <div>
-        <input onChange={changeHandler} name="heading" />
+        <input
+          placeholder="Title"
+          // value={empty}
+          onChange={changeHandler}
+          name="heading"
+        />
         <br />
-        <input onChange={changeHandler} name="note" />
+        <input
+          placeholder="Note"
+          // value={empty}
+          onChange={changeHandler}
+          name="note"
+        />
         <br />
         <button onClick={changeHandler} name="addNote">
           Add Note
@@ -37,6 +88,9 @@ export default function App() {
               <div>
                 <h4>{note.heading}</h4>
                 <p>{note.note}</p>
+                <button id={note.id} onClick={deleteNote}>
+                  Del
+                </button>
               </div>
             </li>
           ))}
